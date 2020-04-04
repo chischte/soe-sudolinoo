08d25c971c95490aa58bda785322ef7348b2f074
Michael Wettstein <chischte@gmx.ch>
2020-04-04 15:17:24 +0200

    BKP uf ugly but working state, generate buttons in controller

--- src/main/java/ch/fxapp/FxController.java
+++ src/main/java/ch/fxapp/FxController.java
@@ -2,10 +2,15 @@ package main.java.ch.fxapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class FxController {
+    int index ;
+    int target = 10;
+
     @FXML
     private Label testlabeltarget;
 
@@ -13,8 +18,30 @@ public class FxController {
     private TextField name;
 
     @FXML
-    protected void onSubmit(ActionEvent event) {
-        testlabeltarget.setText("HAUDI");
+    GridPane button_grid;
+
+    public void initialize() {
+        for (index = 2; index < target ; index++) {
+            addButton();
+        }
+    }
+
+    public void test(){
+        System.out.println("Testing");
+    }
+
+    public void addButton(){
+        Button sound_button = new Button("Button_" + index);
+        button_grid.add(sound_button, index,2);
+    }
+
+    @FXML
+    protected void clickOnGetSudoku(ActionEvent event) {
+        SudokuProvider sudokuProvider = new SudokuProvider();
+        int[][] sudoku = sudokuProvider.getNewSudoku();
+        testlabeltarget.setText(String.valueOf(sudoku[1][1]));
 
     }
+
+
 }
