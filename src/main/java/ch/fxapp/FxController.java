94222c146e7f20926fdf2c4e008ec54fb2bf4fd5
Michael Wettstein <chischte@gmx.ch>
2020-04-05 22:10:18 +0200

    Fix bug 0 fields displayed

--- src/main/java/ch/fxapp/FxController.java
+++ src/main/java/ch/fxapp/FxController.java
@@ -6,23 +6,18 @@ import main.java.ch.controller.SudokuSolver;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class FxController {
-    int index;
-    int target = 10;
+    int sudokuLength = 9;
+
     private Button[][] sudokuFields = new Button[9][9];
 
-    Loader createtLoader1 = new Loader();
-    private int [][] LoaderSudoku1 = createtLoader1.loadSudokuArray();
+    Loader createdLoader1 = new Loader();
+    private int[][] LoaderSudoku1 = createdLoader1.loadSudokuArray();
 
     SudokuSolver Solver1 = new SudokuSolver();
-    private int [][] SolverSudoku1 = Solver1.startsolver();
-
-    //SudokuProvider sudokuProvider = new SudokuProvider();
-    //int[][] sudoku = sudokuProvider.getNewSudoku();
+    private int[][] SolverSudoku1 = Solver1.startsolver();
 
     @FXML
     GridPane sudokuGrid;
@@ -33,20 +28,20 @@ public class FxController {
 
     @FXML
     protected void clickOnGetSudoku(ActionEvent event) {
-        pasteNumbersToSudokuGrid();
+        pastePuzzleNumbersToSudokuGrid();
     }
 
     @FXML
     protected void clickOnSolveSudoku(ActionEvent event) {
-        pasteNumbersToSudokuGridSolved();
+        pasteSolutionToSudokuGrid();
     }
 
 
     public void generateSudokuGrid() {
-        for (int row = 0; row < sudokuFields.length; row++) {
-            for (int col = 0; col < sudokuFields.length; col++) {
+        for (int row = 0; row < sudokuLength; row++) {
+            for (int col = 0; col < sudokuLength; col++) {
                 sudokuFields[row][col] = new Button();
-                sudokuFields[row][col].setStyle("-fx-border-width:1; -fx-border-color: black; -fx-min-width: 33; -fx-min-height: 33");
+                sudokuFields[row][col].setStyle("-fx-border-width:1; -fx-border-color: black; -fx-min-width: 33; -fx-min-height: 33;");
                 /* how to set style class? */
                 sudokuGrid.getChildren().add(sudokuFields[row][col]);
                 GridPane.setConstraints(sudokuFields[row][col], row, col);
@@ -54,30 +49,28 @@ public class FxController {
         }
     }
 
-    public void pasteNumbersToSudokuGrid() {
-        for (int row = 0; row < sudokuFields.length; row++) {
-            for (int col = 0; col < sudokuFields[0].length; col++) {
+    public void pastePuzzleNumbersToSudokuGrid() {
+        for (int row = 0; row < sudokuLength; row++) {
+            for (int col = 0; col < sudokuLength; col++) {
                 // Leave fields with value 0 empty
-                if (LoaderSudoku1[row][col] != 0) {
-                    sudokuFields[row][col].setText(LoaderSudoku1[col][row] + "");
+                int currentNumber = LoaderSudoku1[row][col];
+                if (currentNumber != 0) {
+                    sudokuFields[col][row].setText(Integer.toString(currentNumber));
                 } else {
-                    sudokuFields[row][col].setText(" ");
+                    sudokuFields[col][row].setText("");
                 }
             }
         }
     }
 
-    public void pasteNumbersToSudokuGridSolved() {
-        for (int row = 0; row < sudokuFields.length; row++) {
-            for (int col = 0; col < sudokuFields[0].length; col++) {
-                // Leave fields with value 0 empty
-                if (SolverSudoku1[row][col] != 0) {
-                    sudokuFields[row][col].setText(SolverSudoku1[col][row] + "");
-                } else {
-                    sudokuFields[row][col].setText(" ");
-                }
+
+    public void pasteSolutionToSudokuGrid() {
+        for (int row = 0; row < sudokuLength; row++) {
+            for (int col = 0; col < sudokuLength; col++) {
+                sudokuFields[row][col].setText(SolverSudoku1[col][row] + "");
             }
         }
     }
-
 }
+
+
