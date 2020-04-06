95247ccb7d77972697ee1d2406e30544b2dbded6
joel <56974093+joeliselin@users.noreply.github.com>
2020-04-06 17:35:04 +0200

    Revert "Merge remote-tracking branch 'origin/MichiFirstVersionGui' into MichiFirstVersionGui"
    This reverts commit a34db824947424e20995148cd036c15aaa492358, reversing
    changes made to a6cec0b110940ce3ce1aaa1415564373272cc917.

--- src/main/java/ch/fileloader/JsonLoader.java
+++
@@ -1,4 +0,0 @@
-package main.java.ch.fileloader;
-
-public class JsonLoader {
-}
--- src/main/java/ch/fileloader/JsonLoaderApp.java
+++
@@ -1,8 +0,0 @@
-package main.java.ch.fileloader;
-
-public class JsonLoaderApp {
-    public static void main(String[] args) {
-        JsonLoader jsonLoader = new JsonLoader();
-
-    }
-}
