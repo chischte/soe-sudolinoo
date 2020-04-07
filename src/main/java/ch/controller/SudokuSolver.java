0f1ac95e1e9807c38695a896d769feedca34ff6a
Roland Jaggi <roland.jaggi@vzch.com>
2020-04-07 21:00:24 +0200

    - Refactoring code and optimize - buttons get and solve are working now instead of get the solved result by starting the project

--- src/main/java/ch/controller/SudokuSolver.java
+++ src/main/java/ch/controller/SudokuSolver.java
@@ -4,205 +4,136 @@ import main.java.ch.fileloader.Loader;
 
 public class SudokuSolver {
 
-    public int[][] LoaderSudoku;
-    public int[][] SolvedSudoku;
-    private int GirdsziseSudoku;
+    public int[][] loaderSudoku;
+    public int[][] solvedSudoku;
+    private int gridSizeSudoku;
     private int row = 0;
     private int col = 0;
 
 
-    private boolean isFull() {
+    public int[][] startSolvingSudoku() {
+        Loader createdLoader = new Loader();
+        loaderSudoku = createdLoader.getPuzzle();
+        gridSizeSudoku = loaderSudoku.length;
 
-        System.out.println("Funktion is Full wird ausgeführt");
-        System.out.println(LoaderSudoku[row][col] + " " + "wert nach dem schreiben in dem leeren feld");
+        solver();
 
-        boolean s = true;
-        for (int y = 0; y < GirdsziseSudoku; y++) {
-            for (int x = 0; x < GirdsziseSudoku; x++) {
-                if (LoaderSudoku[x][y] == 0) {
-                    System.out.println("isFull = false");
-                    s = false;
+        return solvedSudoku;
+    }
+
+    private boolean isSudokuFullyFilledOut(int gridSize, int [][] loader) {
+        for (int y = 0; y < gridSize; y++) {
+            for (int x = 0; x < gridSize; x++) {
+                if (loader[x][y] == 0) {
+                    return false;
                 }
-
             }
         }
-        System.out.println("return der funktion isFull");
-        return s;
+
+        return true;
     }
 
-
-    public int[][] startsolver() {
-        // Create Loder
-        Loader createtLoader = new Loader();
-
-        // Set the Loader return Value = the Local two dimentional Array
-        System.out.println("**************************************************************************************");
-        LoaderSudoku = createtLoader.loadSudokuArray();
-        System.out.println("Length is : " + LoaderSudoku.length);
-
-        GirdsziseSudoku = LoaderSudoku.length;
-
-
-
-        System.out.println("Der startsolver startet den solver");
-        if (solver()) {
-            System.out.println("Der return der funktion solve wird gemacht");
-        }
-
-        System.out.println("der return der Startsolver methode erfolgt");
-        return SolvedSudoku;
-    }
-
-    // Jeck if the Actuall Possition is Valid
+    // Check if the actual position is Valid
     public int[] possibleEntries () {
-        int rowEntries = row;
-        int colEntries = col;
-        int k = 0;
-        int l = 0;
         int[] possibilityArray = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-        System.out.println("Array posibilities wurde geöffnet");
+        // Check if the horizontal row and if the vertical col is valid
+        possibilityArray = checkIfHorizontalIsValid(possibilityArray, gridSizeSudoku);
+        possibilityArray = checkIfVerticalIsValid(possibilityArray, gridSizeSudoku);
 
+        // Check if the 3x3 Sudoku rule is valid
+        possibilityArray = isTheThreeByThreeRuleValid(getTheThreeByTreeGrid(row), getTheThreeByTreeGrid(col), possibilityArray);
 
-        //Jeck if the Horizontal row is Valid
-        System.out.println("Jeck if the Horizontal row is Valid");
-        System.out.println(rowEntries + " " + colEntries);
+        // Change the array to the possible numbers
+        possibilityArray = changeToPossibleNumbers(possibilityArray);
+        return possibilityArray;
 
-        for (int y = 0; y < GirdsziseSudoku; y++) {
-            int b = 0;
-            for (int a = 1; a < 10; a++) {
-                if (LoaderSudoku[row][y] == a) {
-                    System.out.println(a);
-                    possibilityArray[b] = 1;
-                }
-                b++;
+    }
+
+    private int[] changeToPossibleNumbers(int[] possibilityArray){
+        int ConversionPossibilityArray = 1;
+        for (int i = 0; i < gridSizeSudoku; i++) {
+            if (possibilityArray[i] == 1) {
+                possibilityArray[i] = 0;
+            } else {
+                possibilityArray[i] = ConversionPossibilityArray;
             }
+            ConversionPossibilityArray++;
         }
 
-        // just for test case
-        System.out.println("nach dem Horizontal test");
-        for (int a = 0; a < GirdsziseSudoku; a++) {
-            System.out.println(possibilityArray[a] + "");
-        }
+        return possibilityArray;
+    }
 
-        // Jeck if the vertical col is valid
-        System.out.println("Jeck if the vertical col is valid");
-        for (int x = 0; x < GirdsziseSudoku; x++) {
-            int b = 0;
-            for (int a = 1; a < 10; a++) {
-                if (LoaderSudoku[x][col] == a) {
-                    System.out.println(a);
-                    possibilityArray[b] = 1;
-                }
-                b++;
-            }
-        }
+    private int[] isTheThreeByThreeRuleValid(int threeByThreeRow, int threeByThreeCol, int[] possibilityArray) {
+        int loader;
+        for (int row = 0; row < 3; row++){
+            for (int col = 0; col < 3; col++){
+                loader = loaderSudoku[threeByThreeRow + row][threeByThreeCol + col];
 
-        // just for test case
-        System.out.println("nach dem vertical test");
-        for (int a = 0; a < GirdsziseSudoku; a++) {
-            System.out.println(possibilityArray[a] + "");
-        }
-
-
-        // Jeck if the 3x3 Sudoku rule is valid
-        System.out.println("Jeck if the 3x3 Sudoku rule is valid");
-
-        if (row >= 0 && row <= 2) {
-            k = 0;
-        } else if (row >= 3 && row <= 5) {
-            k = 3;
-        } else {
-            k = 6;
-        }
-
-        System.out.println(k + " den wert K nach der zuweisung ");
-
-        if (col >= 0 && col <= 2) {
-            l = 0;
-        } else if (col >= 3 && col <= 5) {
-            l = 3;
-        } else {
-            l = 6;
-        }
-        System.out.println(l + " den wert K nach der zuweisung ");
-
-
-        for (int x = 0; x < 3; x++){
-            for (int y = 0; y < 3; y++){
-                int t = k + x;
-                int v = l + y;
-
-//                System.out.println(t + " " + v + " Position von K/l");
-//                System.out.println(LoaderSudoku[t][v] + " wert in K/l");
-                if (LoaderSudoku[t][v] != 0) {
+                if (loader != 0) {
                     for (int a = 1; a < 10; a++) {
-                        int b = a - 1;
-                        if (LoaderSudoku[t][v] == a) {
-                            possibilityArray[b] = 1;
+                        if (loader == a) {
+                            possibilityArray[a-1] = 1;
                             break;
                         }
                     }
                 }
             }
         }
-
-        System.out.println("nach dem 3x3");
-        for (int a = 0; a < GirdsziseSudoku; a++) {
-            System.out.println(possibilityArray[a] + "");
-        }
-
-
-        System.out.println("Umwandlung");
-        // Das Array wieder umwandeln von den einsen in Zahlen die möglichwähren
-        int UmwandlungpossibilityArray = 1;
-        for (int b = 0; b < GirdsziseSudoku; b++) {
-            if (possibilityArray[b] == 1) {
-                possibilityArray[b] = 0;
-            } else {
-                possibilityArray[b] = UmwandlungpossibilityArray;
-            }
-            UmwandlungpossibilityArray++;
-        }
-
-        for (int a = 0; a < GirdsziseSudoku; a++) {
-            System.out.println(possibilityArray[a] + "");
-        }
-        System.out.println("Das Possibility Array wurde von der Funktion zurück gegeben");
         return possibilityArray;
     }
 
+    private int[] checkIfVerticalIsValid(int[] possibilityArray, int gridSize) {
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 1; j < 10; j++) {
+                if (loaderSudoku[i][col] == j) {
+                    possibilityArray[j-1] = 1;
+                }
+            }
+        }
+
+        return possibilityArray;
+    }
+
+    private int[] checkIfHorizontalIsValid(int[] possibilityArray, int gridSize) {
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 1; j < 10; j++) {
+                 if (loaderSudoku[row][i] == j) {
+                    possibilityArray[j-1] = 1;
+                }
+            }
+        }
+
+        return possibilityArray;
+    }
+
+    private int getTheThreeByTreeGrid(int number) {
+        if (number >= 0 && number <= 2) {
+            return 0;
+        } else if (number >= 3 && number <= 5) {
+            return 3;
+        } else {
+            return 6;
+        }
+    }
 
     public boolean solver () {
         int rowsolver = 0;
         int colsolver = 0;
-        int[] possibilityArraysolver = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] possibilityArraySolver = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-        // Jeck if Function is running
-        System.out.println("Sudoku solver wird ausgeführt");
-        System.out.println("die aktuellen werte row und col von dem solver");
-        System.out.println(rowsolver + "" + colsolver);
-
-
-        if (isFull()) {
+        if (isSudokuFullyFilledOut(gridSizeSudoku, loaderSudoku)) {
             // this is the place where the solved sudoku goes to the Gui
-            SolvedSudoku = LoaderSudoku;
-            System.out.println("Das Sudoku ist voll --------------------------------------");
-        return true;
+            solvedSudoku = loaderSudoku;
+            return true;
 
         } else {
-
             search:
             {
-                System.out.println("Das Sudoku ist nicht voll");
-                System.out.println("finde das erste leere Feld");
-                // find first empty field
-                for (int x = 0; x < GirdsziseSudoku; x++) {
-                    for (int y = 0; y < GirdsziseSudoku; y++) {
-                        if (LoaderSudoku[x][y] == 0) {
-                            System.out.println("Das erste leere feld gefunden");
-                            System.out.println(LoaderSudoku[x][y] + " wert in dem leeren Feld ");
-                            System.out.println(x + " " + y);
+                // detect first empty field
+                for (int x = 0; x < gridSizeSudoku; x++) {
+                    for (int y = 0; y < gridSizeSudoku; y++) {
+                        if (loaderSudoku[x][y] == 0) {
                             row = x;
                             col = y;
                             break search;
@@ -211,51 +142,25 @@ public class SudokuSolver {
                 }
             }
 
-
-            System.out.println(row + " " + col);
             rowsolver = row;
             colsolver = col;
 
-            System.out.println("Array posibleEntries wird aufgerufen");
-            possibilityArraysolver = possibleEntries();
+            possibilityArraySolver = possibleEntries();
 
-            System.out.println("Das erste leere Feld befindet sich hir");
-            System.out.println(row + " " + col);
-
-            System.out.println("die aktuellen werte row und col von dem solver");
-            System.out.println(rowsolver + "" + colsolver);
-
-            System.out.println("anzeige Solver Possibiliti Array");
-            for (int a = 0; a < GirdsziseSudoku; a++) {
-                System.out.println(possibilityArraysolver[a] + "");
-            }
-
-            for (int a = 0; a < GirdsziseSudoku; a++) {
-                if (isFull()) {
+            for (int i = 0; i < gridSizeSudoku; i++) {
+                if (isSudokuFullyFilledOut(gridSizeSudoku, loaderSudoku)) {
                     break;
                 }
-                System.out.println("steppen durch die möglichkeiten");
-                System.out.println(a + " " + "den wert wollen wir aus dem arrap vergleichen ");
-                System.out.println(rowsolver + " " + colsolver + " Feld in dem wir uns befinden");
-                if (possibilityArraysolver[a] != 0) {
-                    System.out.println("Die folgende Zahl wird in das feld geschrieben");
-                    System.out.println(possibilityArraysolver[a]);
-                    System.out.println(LoaderSudoku[rowsolver][colsolver] + "wert vor dem schreiben in dem leeren feld");
 
-                    LoaderSudoku[rowsolver][colsolver] = possibilityArraysolver[a];
-
-                    System.out.println(LoaderSudoku[rowsolver][colsolver] + " " + "wert nach dem schreiben in dem leeren feld");
-                    System.out.println(rowsolver + " " + colsolver + "wert nach dem schreiben in dem leeren feld");
-
-
+                if (possibilityArraySolver[i] != 0) {
+                    loaderSudoku[rowsolver][colsolver] = possibilityArraySolver[i];
+                    // ToDo get out of the loop when solver returns true
                     solver();
 
-                } else if (a == 8) {
-                    System.out.println("Das Feld hat keine Lösung gehe einen Schritt zurück");
-                    LoaderSudoku[rowsolver][colsolver] = 0;
+                } else if (i == 8) {
+                    loaderSudoku[rowsolver][colsolver] = 0;
                 }
             }
-
         }
 
         return false;
--- src/main/java/ch/fxapp/FxController.java
+++ src/main/java/ch/fxapp/FxController.java
@@ -9,15 +9,11 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 public class FxController {
-    int sudokuLength = 9;
 
-    private Button[][] sudokuFields = new Button[9][9];
-
-    Loader createdLoader1 = new Loader();
-    private int[][] LoaderSudoku1 = createdLoader1.loadSudokuArray();
-
-    SudokuSolver Solver1 = new SudokuSolver();
-    private int[][] SolverSudoku1 = Solver1.startsolver();
+    Loader createdLoader = new Loader();
+    private int[][] loaderSudoku = createdLoader.getPuzzle();
+    int sudokuLength = loaderSudoku.length;
+    private Button[][] sudokuFieldButtons = new Button[sudokuLength][sudokuLength];
 
     @FXML
     GridPane sudokuGrid;
@@ -33,17 +29,19 @@ public class FxController {
 
     @FXML
     protected void clickOnSolveSudoku(ActionEvent event) {
-        pasteSolutionToSudokuGrid();
+        SudokuSolver sudokuSolver = new SudokuSolver();
+        int[][] solverSudoku = sudokuSolver.startSolvingSudoku();
+        pasteSolutionToSudokuGrid(solverSudoku);
     }
 
     public void generateSudokuGrid() {
         for (int row = 0; row < sudokuLength; row++) {
             for (int col = 0; col < sudokuLength; col++) {
-                sudokuFields[row][col] = new Button();
-                sudokuFields[row][col].setStyle("-fx-border-width:1; -fx-border-color: black; -fx-min-width: 33; -fx-min-height: 33;");
+                sudokuFieldButtons[row][col] = new Button();
+                sudokuFieldButtons[row][col].setStyle("-fx-border-width:1; -fx-border-color: black; -fx-min-width: 33; -fx-min-height: 33;");
                 /* how to set style class? */
-                sudokuGrid.getChildren().add(sudokuFields[row][col]);
-                GridPane.setConstraints(sudokuFields[row][col], row, col);
+                sudokuGrid.getChildren().add(sudokuFieldButtons[row][col]);
+                GridPane.setConstraints(sudokuFieldButtons[row][col], row, col);
             }
         }
     }
@@ -52,21 +50,20 @@ public class FxController {
         for (int row = 0; row < sudokuLength; row++) {
             for (int col = 0; col < sudokuLength; col++) {
                 // Leave fields with value 0 empty
-                int currentNumber = LoaderSudoku1[row][col];
+                int currentNumber = loaderSudoku[row][col];
                 if (currentNumber != 0) {
-                    sudokuFields[col][row].setText(Integer.toString(currentNumber));
+                    sudokuFieldButtons[col][row].setText(Integer.toString(currentNumber));
                 } else {
-                    sudokuFields[col][row].setText("");
+                    sudokuFieldButtons[col][row].setText("");
                 }
             }
         }
     }
 
-
-    public void pasteSolutionToSudokuGrid() {
+    public void pasteSolutionToSudokuGrid(int[][] solverSudoku) {
         for (int row = 0; row < sudokuLength; row++) {
             for (int col = 0; col < sudokuLength; col++) {
-                sudokuFields[row][col].setText(SolverSudoku1[col][row] + "");
+                sudokuFieldButtons[row][col].setText(solverSudoku[col][row] + "");
             }
         }
     }
