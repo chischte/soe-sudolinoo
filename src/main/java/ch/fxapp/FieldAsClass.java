f57d07ae7f51ebf60b44fef349c204b20f2ea5ef
Michael Wettstein <chischte@gmx.ch>
2020-04-12 02:39:23 +0200

    Add experimental class FieldAsClass

--- src/main/java/ch/fxapp/FieldAsClass.java
+++ src/main/java/ch/fxapp/FieldAsClass.java
@@ -4,13 +4,20 @@ import java.util.*;
 
 public class FieldAsClass {
     private boolean isSolved = false;
-    private int columNo;
+    private int columnNo=2;
     private int rowNo;
+
+    public void setFieldValue(int fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+
+    private int fieldValue;
     private int sectorNo;
     private int remainingPossibilites = 9;
     private HashSet<Integer> possibilities = new HashSet<>();
 
 
+    // THIS IS THE CONSTRUCTOR
     public FieldAsClass() {
         Integer arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
@@ -26,4 +33,39 @@ public class FieldAsClass {
         this.possibilities.remove(numberToRemove);
     }
 
+    public boolean isSolved() {
+        return this.isSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        isSolved = solved;
+        System.out.println("This Field is solved = " + solved);
+    }
+
+    public int getColumnNo() {
+        return columnNo;
+    }
+
+    public void setColumNo(int columnNo) {
+        System.out.println("Sector= " + columnNo);
+        this.columnNo = columnNo;
+    }
+
+    public int getRowNo() {
+        return rowNo;
+    }
+
+    public void setRowNo(int rowNo) {
+        System.out.println("This Field is on row number= " + rowNo);
+        this.rowNo = rowNo;
+    }
+
+    public int getSectorNo() {
+        return sectorNo;
+    }
+
+    public void setSectorNo(int sectorNo) {
+        System.out.println("Sector= " + sectorNo);
+        this.sectorNo = sectorNo;
+    }
 }
--- src/main/java/ch/fxapp/FieldClassMain.java
+++ src/main/java/ch/fxapp/FieldClassMain.java
@@ -1,33 +1,83 @@
 package main.java.ch.fxapp;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import org.w3c.dom.html.HTMLFieldSetElement;
 
 public class FieldClassMain {
-    public static void main(String[] args){
-        FieldAsClass fieldAsClass= new FieldAsClass();
-        fieldAsClass.printPossibleNo();
-        fieldAsClass.removePossibleNo(5);
-        fieldAsClass.printPossibleNo();
+    // This is an array containing an object for every sudoku field
+    static FieldAsClass[] fieldAsClassArray = new FieldAsClass[81];
+
+    static int puzzle[][] = {
+            {8, 0, 1, 0, 0, 3, 9, 0, 6},
+            {0, 0, 9, 0, 0, 7, 8, 5, 0},
+            {2, 5, 0, 1, 0, 0, 4, 7, 0},
+            {5, 0, 0, 0, 6, 1, 7, 0, 4},
+            {7, 6, 0, 8, 3, 0, 0, 0, 0},
+            {0, 3, 2, 0, 0, 0, 0, 0, 0},
+            {0, 2, 0, 0, 1, 9, 5, 0, 0},
+            {0, 0, 5, 0, 0, 0, 3, 0, 2},
+            {0, 0, 0, 4, 5, 2, 1, 9, 7}
+    };
+
+    static int[][] unsolvedSudoku = new int[9][9];
+    static int sudokuLength = unsolvedSudoku.length;
+
+    public static void main(String[] args) {
+        configureAllFields(puzzle);
     }
 
-    // CREATE SUDOKU FIELD ARRAY
-    public static void createFieldArray(){
-        //FieldAsClass[] fieldAsClassArray = new FieldAsClass[];
 
+    // CONFIGURE ALL FIELDS
+    public static void configureAllFields(int[][] sudokuArray) {
+        int currentSector = 1; //there are 9 sectors
+        for (int row = 0; row < sudokuLength; row++) {
+            for (int col = 0; col < sudokuLength; col++) {
+                if (col < 3) {
+                    currentSector = 1;
+                } else if (col < 6) {
+                    currentSector = 2;
+                } else {
+                    currentSector = 3;
+                }
+                if (row < 3) {
+                    currentSector += 0;
+                }
+                else if (row < 6) {
+                    currentSector += 3;
+                } else {
+                    currentSector += 6;
+                }
+                int currentValue = sudokuArray[row][col];
+                int currentField = row * sudokuLength + col;
+                fieldAsClassArray[currentField] = new FieldAsClass();
+                fieldAsClassArray[currentField].setSectorNo(currentSector);
+                if (currentValue != 0) {
+                    fieldAsClassArray[currentField].setSolved(true);
+                    fieldAsClassArray[currentField].setFieldValue(currentValue);
+                } else {
+                    fieldAsClassArray[currentField].setSolved(false);
+                }
+
+            }
+        }
     }
-
-    // ENTER DEFINED FIELDS FROM PUZZLE
-
-    // REMOVE POSSIBILITIES IN SECTOR
-
-    //REMOVE POSSIBILITIES IN COLUMN
 
     //REMOVE POSSIBILITIES IN ROW
 
+    //REMOVE POSSIBILITIES IN COLUMN
+
+    // REMOVE POSSIBILITIES IN SECTOR
+
     // FIND FIELD WITH THE LEAST POSSIBILITIES
-    // ...AND MAKE IT "SOLVED"
+    // ...SELECT A NUMBER
+    // ...AND MARE IT "SOLVED"
 
     // CHECK IF ALL FIELDS ARE SOLVED
     // ...AND REPEAT UNTIL ALL FIELDS ARE SOLVED
+
+    // WRITE ARRAY OF OBJECTS TO SOLVED SUDOKU ARRAY
+    // ...RETURN THE SOLVED SUDOK ARRAY
+
 
 }
